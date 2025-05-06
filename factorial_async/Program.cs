6c6c7558594fc19d5fc3a4c113e1424d48968647
Artemi <133803304+Artemiii990@users.noreply.github.com>
2025-05-06 17:54:11 +0300

    initial comit by Artemka

--- factorial_async/Program.cs
+++ factorial_async/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static async Task Main(string[] args) //використовуємо async для асинхронності
+    {
+        Console.WriteLine("Введіть число для обчислення факторіалу:");
+        
+        string input = Console.ReadLine();
+        
+        if (int.TryParse(input, out int number) && number >= 0)
+        {
+            // Виклик асинхронного методу для обчислення факторіалу
+            long factorial = await CalculateFactorialAsync(number);
+            Console.WriteLine($"Факторіал числа {number} = {factorial}");
+        }
+        else
+        {
+            Console.WriteLine("Будь ласка, введіть невід'ємне ціле число.");
+        }
+    }
+    
+    // метод, який запускає цей код в окремому потоці через метод .Run
+    public static Task<long> CalculateFactorialAsync(int number)
+    {
+        return Task.Run(() => CalculateFactorial(number));
+    }
+    
+    // основний метод, який фактично створює сам цикл знаходження факторіалу
+    public static long CalculateFactorial(int number)
+    {
+        if (number == 0 && number == 1)
+        {
+            return 1;
+        }
+
+        long result = 1;
+        
+        for (int i = 2; i <= number; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+}
